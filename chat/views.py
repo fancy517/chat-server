7c26b7b1c5cb50448390d28d7947b3df5f7a217c
Rene Robollo <rdoublegroup@mail.com>
2020-10-19 11:56:30 -0400

    implement raw query to join table

--- chat/views.py
+++ chat/views.py
@@ -8,6 +8,15 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Q
+from django.db import connection
+
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
 
 @csrf_exempt
 def getMessages(request):
@@ -18,13 +27,22 @@ def getMessages(request):
     page = payload.get('page')
     offset = payload.get('offset')
     if (room_id):
-    	messages = Message.objects.filter(room_id=room_id)
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT chat_message.id, chat_message.text, chat_message.created_at, chat_message.message_type, chat_message.sender, users.username FROM chat_message INNER JOIN users ON (cast(chat_message.sender as uuid) = users.user_id) WHERE chat_message.room_id = %s ORDER BY chat_message.created_at DESC;", [room_id])
+            messages = dictfetchall(cursor)
+            cursor.close()
+            connection.close()
     else:
-    	messages = Message.objects.filter(sender=user1, receiver=user2) | Message.objects.filter(sender=user2, receiver=user1)
+        with connection.cursor() as cursor:
+            queryString = f"SELECT chat_message.id, chat_message.text, chat_message.created_at, chat_message.message_type, chat_message.sender, users.username FROM chat_message INNER JOIN users ON (cast(chat_message.sender as uuid) = users.user_id) WHERE (chat_message.sender = '{user1}' AND chat_message.receiver = '{user2}') OR (chat_message.sender = '{user2}' AND chat_message.receiver = '{user1}') ORDER BY chat_message.created_at DESC;";
+            cursor.execute(queryString)
+            messages = dictfetchall(cursor)
+            cursor.close()
+            connection.close()
 
     json_response = {
-        'messages': json.loads(serializers.serialize('json', messages.order_by('-created_at')[(page - 1) * offset : page * offset] )),
-        'count': messages.count()
+        'messages': (messages)[(page - 1) * offset : page * offset],
+        'count': len(list(messages))
     }
 
     return JsonResponse(json_response)
